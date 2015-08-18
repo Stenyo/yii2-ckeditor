367c7cefe31df0939af34a7e9d2304d7b8849b5d
stenyo <stenyo.borges@gmail.com>
2015-08-18 18:00:01 -0300

    external ckeditor composer

--- src/CKEditorAsset.php
+++ src/CKEditorAsset.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * @copyright Copyright (c) 2013-15 2amigOS! Consulting Group LLC
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
+
 namespace dosamigos\ckeditor;
 
 use yii\web\AssetBundle;
@@ -16,20 +18,15 @@ use yii\web\AssetBundle;
  * @link http://www.2amigos.us/
  * @package dosamigos\ckeditor
  */
-class CKEditorAsset extends AssetBundle
-{
+class CKEditorAsset extends AssetBundle {
+
+    public $sourcePath = '@ckeditor/ckeditor';
     public $js = [
         'ckeditor.js',
-        'adapters/jquery.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\web\JqueryAsset'
     ];
 
-    public function init()
-    {
-        $this->sourcePath = __DIR__ . '/assets/ckeditor';
-        parent::init();
-    }
 }
